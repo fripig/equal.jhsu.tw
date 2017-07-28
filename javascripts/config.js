a064c789138a192e2ad220b79757682b482d9599
fripig <fripig@gmail.com>
2017-07-28 21:23:56 +0800

    'done'

---
+++ javascripts/config.js
@@ -0,0 +1,3 @@
+Config = {};
+Config.url = "http://hands.spotlights.news/";
+Config.fb_app_id = '353808838309028';
---
+++ javascripts/util.js
@@ -0,0 +1,148 @@
+(function () {
+    var Util;
+
+    Util = (function () {
+        var pix2int;
+
+        function Util() {
+        }
+
+        Util.prototype.urlToBlob = function (url, callback) {
+            var xhr;
+
+            xhr = new XMLHttpRequest;
+            xhr.open('GET', url, true);
+            xhr.responseType = 'arraybuffer';
+            xhr.onload = function (e) {
+                var arrayBufferView, blob;
+
+                arrayBufferView = new Uint8Array(this.response);
+                blob = new Blob([arrayBufferView], {
+                    type: 'image/png'
+                });
+                return callback(blob);
+            };
+            return xhr.send();
+        };
+
+        Util.prototype.getBackgroundSize = function (string) {
+            var size;
+
+            size = string.split(' ');
+            return [pix2int(size[0]), pix2int(size[1])];
+        };
+
+        Util.prototype.getBackgroundPosition = function (string) {
+            var position;
+
+            position = string.split(' ');
+            return [pix2int(position[0]), pix2int(position[1])];
+        };
+
+        Util.prototype.getBackgroundCenterPoint = function (size, position) {
+            return [size[0] * 0.5 + position[0], size[1] * 0.5 + position[1]];
+        };
+
+        Util.prototype.getImgSize = function (src) {
+            var newImg;
+
+            newImg = new Image();
+            newImg.src = src;
+            return [newImg.width, newImg.height];
+        };
+
+        Util.prototype.getBackgroundImageUrl = function (element) {
+            var url;
+
+            url = element.css('background-image');
+            return url.replace(/^url\(["']?/, '').replace(/["']?\)$/, '');
+        };
+
+        pix2int = function (string) {
+            return parseFloat(string.replace('px', ''));
+        };
+
+        Util.prototype.downloadByBase64 = function (base64, callback) {
+            return $util.uploadBase64(base64, function (url, w) {
+                ga('download', 'pic', $('input[name=template]:checked').val(),url);
+                if ($util.isDesktop() && !$util.isSafari()) {
+                    return $util.urlToBlob(url, function (blob) {
+                        w.close();
+                        return saveAs(blob, 'avatar.png');
+                    });
+                } else {
+                    return callback(url, w);
+                }
+
+            });
+        };
+
+        Util.prototype.uploadBase64 = function (base64, callback) {
+            var directDownload, endpoing, w, clientId;
+
+            directDownload = false;
+            if ($util.isFBWebview()) {
+                w = window;
+            } else {
+                w = window.open("/waiting.html", "wait", "width=500, height=500, menubar=no, resizable=no, scrollbars=no, status=no, titlebar=no, toolbar=no");
+            }
+            // endpoing = "http://iing.tw/badges.json"
+            // return $.post(endpoing, {
+            //   data: base64
+            // }, function(result) {
+            //   return callback(result.url, w);
+            // });
+            var timestamp = +(new Date());
+            var objectName = timestamp + '_' + Math.random() + '.png';
+            var data  = new FormData();
+            data.append('image',base64.replace('data:image/png;base64,', ''))
+            return $.ajax({
+                "url": "https://api.imgur.com/3/image",
+                "method": "POST",
+                'cache': false,
+                "headers": {
+                    "authorization": "Client-ID d8b6ccf7579e48e"
+                },
+                "processData": false,
+                "contentType": false,
+                "mimeType": "multipart/form-data",
+                "data": data,
+                success: function (result) {
+                    result = JSON.parse(result)
+                    callback(result.data.link, w)
+                }
+            })
+        };
+
+        Util.prototype.resizeWindow = function (w, width, height) {
+            if (w.outerWidth) {
+                return w.resizeTo(width + (w.outerWidth - w.innerWidth), height + (w.outerHeight - w.innerHeight));
+            } else {
+                w.resizeTo(500, 500);
+                return w.resizeTo(width + (500 - w.document.body.offsetWidth), height + (500 - w.document.body.offsetHeight));
+            }
+        };
+
+        Util.prototype.isFBWebview = function () {
+            return navigator.userAgent.match(/FB/);
+        };
+
+        Util.prototype.isDesktop = function () {
+            return WURFL.form_factor === 'Desktop';
+        };
+
+        Util.prototype.isSafari = function () {
+            return /^((?!chrome|android).)*safari/i.test(navigator.userAgent);
+        };
+
+        return Util;
+
+    })();
+
+    window.$util = new Util();
+
+    window.xx = function (v) {
+        return console.log(v);
+    };
+
+}).call(this);
